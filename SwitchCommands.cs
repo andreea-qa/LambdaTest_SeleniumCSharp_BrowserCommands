4d0c2791688bdfa5e4367e2640d98a96f8bb3adf
andreea-qa <andreea.draniceanu@gmail.com>
2023-03-30 08:33:37 +0300

    Added tests for alerts

--- SwitchCommands.cs
+++ SwitchCommands.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,8 +28,30 @@ namespace SeleniumCSharp_BrowserCommands
         [Test]
         public void DismissAlert()
         {
+            // Navigate to the desired page
             driver.Navigate().GoToUrl("https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo");
 
-;        }
+            // Identify the button to click
+            driver.FindElement(By.XPath("//div[contains(text(),'Java Script Alert Box')]/following-sibling::p/button")).Click();
+
+            // Dismiss the alert
+            driver.SwitchTo().Alert().Dismiss();
+        }
+
+        [Test]
+        public void SendKeysAndAcceptAlert()
+        {
+            // Navigate to the desired page
+            driver.Navigate().GoToUrl("https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo");
+
+            // Identify the button to click
+            driver.FindElement(By.XPath("//div[contains(text(),'Java Script Alert Box')]/following-sibling::p/button")).Click();
+
+            // Enter text in the alert box
+            driver.SwitchTo().Alert().SendKeys("It's me, Mario"); 
+
+            // Accept the alert
+            driver.SwitchTo().Alert().Accept();
+        }
     }
 }
